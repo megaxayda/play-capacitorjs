a9e7ea69978528b15667898b01c79e5732585efe
Tai Pham <tai@newlogic.com>
2022-12-07 14:50:28 +0700

    init plugin orientation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,11 @@ const config: CapacitorConfig = {
   appId: 'com.tai.capa',
   appName: 'play-capa',
   webDir: 'dist',
-  bundledWebRuntime: false
+  bundledWebRuntime: false,
+  server: {
+    url: 'http://192.168.28.145:3000',
+    cleartext: true,
+  },
 };
 
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,67 @@
 import './App.css';
 
-import React, { useState } from 'react';
+import { Toast } from '@capacitor/toast';
+import React, { useEffect, useState } from 'react';
 
 import logo from './logo.svg';
+import { ScreenOrientation } from './plugins/screen-orientation';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [orientation, setOrientation] = useState<string>('');
+
+  useEffect(() => {
+    ScreenOrientation.addListener('screenOrientationChange', (res) =>
+      setOrientation(res.type),
+    );
+
+    ScreenOrientation.orientation().then((res) => setOrientation(res.type));
+
+    return () => {
+      ScreenOrientation.removeAllListeners();
+    };
+  }, []);
+
+  const showHelloToast = async () => {
+    await Toast.show({
+      text: 'Hello!',
+    });
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="header">
-          🚀 Vite + React + Typescript 🤘 & <br />
-          Eslint 🔥+ Prettier
+      {orientation}
+      <div className="body">
+        <button onClick={() => setCount((count) => count + 1)}>
+          🪂 Click me : {count}
+        </button>
+        <button onClick={showHelloToast}>showHelloToast</button>
+
+        <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
+
+        <p>
+          Mess up the code in <code>App.tsx </code> and save the file.
         </p>
-
-        <div className="body">
-          <button onClick={() => setCount((count) => count + 1)}>
-            🪂 Click me : {count}
-          </button>
-
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
-
-          <p>
-            Mess up the code in <code>App.tsx </code> and save the file.
-          </p>
-          <p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="App-link"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Vite Docs
-            </a>
-          </p>
-        </div>
-      </header>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
